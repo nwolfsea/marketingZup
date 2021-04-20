565a555d445522089e4beb99325a46bb8b0f68ab
natalia.palota <natalia.palota@zup.com.br>
2021-04-20 16:32:33 -0300

    Criar filtro de produto para contatos

---
+++ src/main/java/com/zup/br/MarketingZup/dtos/FitrarContatosDTO.java
@@ -0,0 +1,10 @@
+package com.zup.br.MarketingZup.dtos;
+
+import com.zup.br.MarketingZup.model.Produto;
+
+import java.util.List;
+
+public class FitrarContatosDTO {
+
+    private List<Produto> produtos;
+}
--- src/main/java/com/zup/br/MarketingZup/repositories/ContatoRepository.java
+++ src/main/java/com/zup/br/MarketingZup/repositories/ContatoRepository.java
@@ -1,7 +1,12 @@
 package com.zup.br.MarketingZup.repositories;
 
 import com.zup.br.MarketingZup.model.Contato;
+import com.zup.br.MarketingZup.model.Produto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ContatoRepository extends CrudRepository<Contato, String> {
+
+    Iterable<Contato> findByProduto(List<Produto> produtos);
 }
