9811a974a1af72147431d649888585e16bb3da89
natalia.palota <natalia.palota@zup.com.br>
2021-04-20 14:03:09 -0300

    Criar Repository

---
+++ src/main/java/com/zup/br/MarketingZup/service/CategoriaService.java
@@ -0,0 +1,7 @@
+package com.zup.br.MarketingZup.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoriaService {
+}
---
+++ src/main/java/com/zup/br/MarketingZup/service/ContatoService.java
@@ -0,0 +1,9 @@
+package com.zup.br.MarketingZup.service;
+
+import org.springframework.stereotype.Service;
+//Criar crud
+//Create, read, update, delete
+
+@Service
+public class ContatoService {
+}
