51d5a94160ed8a4689604f9201669db124de726f
natalia.palota <natalia.palota@zup.com.br>
2021-04-20 13:45:14 -0300

    Criar packages

---
+++ src/main/java/com/zup/br/MarketingZup/MarketingZupApplication.java
@@ -0,0 +1,13 @@
+package com.zup.br.MarketingZup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketingZupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketingZupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zup/br/MarketingZup/MarketingZupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zup.br.MarketingZup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketingZupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
