4a4609ef70c2ad5467e2030cde4d156a262d8a0d
natalia.palota <natalia.palota@zup.com.br>
2021-04-26 16:39:29 -0300

    Inserir configurações de segurança

---
+++ src/main/java/com/zup/br/MarketingZup/configurations/ConfiguracaoDeSeguranca.java
@@ -0,0 +1,46 @@
+package com.zup.br.MarketingZup.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
+
+    private static final String[] PUBLIC_MATCHERS_GET = {
+            "/categorias/**",
+            "/contatos/**",
+            "/produtos/**"
+    };
+
+    private static final String[] PUBLIC_MATCHERS_POST = {
+            "/categorias/**",
+            "/contatos/**",
+            "/produtos/**"
+    };
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.cors();
+
+        http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+                .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
+                .anyRequest().authenticated();
+    }
+
+    @Bean
+    CorsConfigurationSource configuracaoDeCors(){
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
+}
